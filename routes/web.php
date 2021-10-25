abe6dd068c6177dcb42a73d03e567c3a0b41fb91
parix1999 <parisottoedoardo@gmail.com>
2021-10-25 15:54:14 +0200

    installazioni varie

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('components.index');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
